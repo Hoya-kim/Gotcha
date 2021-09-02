efcce0ea2b6401259deb4883967bb63727813141
Hoya-kim <1203play@gmail.com>
2021-09-02 20:19:18 +0900

    chore: add additional babel configures

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,17 @@ module.exports = {
         exclude: /node_modules/,
         type: "asset",
       },
+      {
+        test: /\.js$/,
+        include: path.join(__dirname),
+        exclude: /(node_modules)|(dist)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
     ],
   },
   plugins: [
